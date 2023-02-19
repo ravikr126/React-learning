f31cbced379614962239865c7dddfa68b424a215
Ravi Kumar <ravi14577@gmail.com>
2023-02-19 09:46:07 +0530

    learning commit 19-2-23

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+
+
+const App =() =>{
+    
+return (
+    <>
+    <h1> hii</h1>
+    <input type="text" placeholder="Enter ypur name" val  />
+    <button>Clicke me</button>
+    </>
+);
+};
+
+export default App;
