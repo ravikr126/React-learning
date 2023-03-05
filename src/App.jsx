cac149c40a11171d01ffaf72d27bbdbc4157b7a7
Ravi Kumar <ravi14577@gmail.com>
2023-03-06 00:32:07 +0530

    learning

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,22 @@ const App =() =>{
         console.log(event.target.value);
         setName(event.target.value);
     };
-    const onSubmit = () =>{
+    const onSubmits = (event) =>{
+        event.preventDefault();
         setFullName(name);
     }
 return (
     <>
-    <div>
-    <h1> Hello {fullName}</h1>
-    <input type="text" placeholder="Enter your name" onChange={inputEvent}
-    value={name}  />
-    <button onClick={onSubmit} >Click me</button>
+    <div className="main_div">
+    <form onSubmit={onSubmits}>
+        <div>
+                <h1> Hello {fullName}</h1>
+                <input type="text" placeholder="Enter your name" onChange={inputEvent}
+                value={name}  />
+                <br />
+                <button>Click me</button>
+        </div>
+    </form>
     </div>
     </>
 );
